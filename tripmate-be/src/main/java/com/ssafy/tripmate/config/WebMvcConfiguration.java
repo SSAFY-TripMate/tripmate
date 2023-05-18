c7e52bff55b8b5a522588325ea6b6efd0b649c2e
leeyongjun <jjuny7712@gmail.com>
2023-05-18 13:20:08 +0900

    Init: 프로젝트 생성

---
+++ tripmate-be/src/main/java/com/ssafy/tripmate/config/WebMvcConfiguration.java
@@ -0,0 +1,12 @@
+package com.ssafy.tripmate.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+//@MapperScan(basePackages = {"com.ssafy.**.mapper"})
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+
+}
