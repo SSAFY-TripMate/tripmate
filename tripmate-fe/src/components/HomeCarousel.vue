8d6adedd8f7565b728652a21fef4716f45ff5482
leeyongjun <jjuny7712@gmail.com>
2023-05-18 17:39:40 +0900

    Feat: 메인 페이지 Carousel 구현

---
+++ tripmate-fe/src/components/HomeCarousel.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="carousel mt-4">
+        <b-carousel
+            id="carousel-fade"
+            style="text-shadow: 0px 0px 2px #000"
+            fade
+            indicators
+            img-width="1200"
+            img-height="350"
+        >
+            <b-carousel-slide
+                img-src="https://picsum.photos/1200/350/?image=10"
+            ></b-carousel-slide>
+            <b-carousel-slide
+                caption="두번째 슬라이드"
+                img-src="https://picsum.photos/1200/350/?image=12"
+            ></b-carousel-slide>
+            <b-carousel-slide
+                img-src="https://picsum.photos/1200/350/?image=22"
+            ></b-carousel-slide>
+        </b-carousel>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HomeCarousel",
+    components: {},
+};
+</script>
+
+<style scope>
+.carousel {
+    display: flex;
+    max-width: 1200px;
+    height: 330px;
+    padding: 0 20px;
+    margin: 0 auto;
+    box-sizing: inherit;
+}
+
+img {
+    border-radius: 40px 40px 40px 40px;
+}
+
+b-button {
+    color: aqua;
+}
+</style>
