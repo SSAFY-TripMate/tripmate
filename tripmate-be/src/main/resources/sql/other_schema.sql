ab3b092a0bb5542043c5939a653cd93b64ae5a85
leeyongjun <jjuny7712@gmail.com>
2023-05-18 19:29:00 +0900

    Chore: SQL 스키마 및 MyBatis 설정

---
+++ tripmate-be/src/main/resources/sql/other_schema.sql
@@ -0,0 +1,54 @@
+CREATE TABLE IF NOT EXISTS `member` (
+  `member_no` int PRIMARY KEY auto_increment,
+  `id` varchar(32) NOT NULL unique,
+  `password` varchar(255) NOT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `gender` char(1) NOT NULL CHECK(gender IN ('M', 'F')),
+  `birth` timestamp NOT NULL,
+  `created_time` timestamp DEFAULT current_timestamp,
+  `token` varchar(1000) DEFAULT null
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `mate` (
+  `mate_no` int PRIMARY KEY auto_increment,
+  `sido_code` int,
+  `start_date` timestamp,
+  `end_date` timestamp,
+  `preference_no` int,
+  `capacity` int,
+  `contact` varchar(1000),
+  `title` varchar(1000) NOT NULL,
+  `content` text NOT NULL,
+  `member_no` int NOT NULL,
+  `hit` int DEFAULT 0,
+  `created_time` timestamp DEFAULT current_timestamp,
+  FOREIGN KEY (sido_code) REFERENCES sido(sido_code),
+  FOREIGN KEY (member_no) REFERENCES `member`(member_no) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `mate_thumbnail` (
+  `mate_thumbnail_no` int PRIMARY KEY auto_increment,
+  `mate_no` int NOT NULL,
+  `image_folder` varchar(1000) NOT NULL,
+  `image_origin_name` varchar(255) NOT NULL,
+  `image_save_name` varchar(255) NOT NULL,
+  `image_type` varchar(255) NOT NULL,
+  FOREIGN KEY (mate_no) REFERENCES mate(mate_no)
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `mate_comment` (
+  `mate_comment_no` int PRIMARY KEY auto_increment,
+  `mate_no` int NOT NULL,
+  `content` text NOT NULL,
+  `member_no` int NOT NULL,
+  `created_time` timestamp DEFAULT current_timestamp,
+  FOREIGN KEY (mate_no) REFERENCES mate(mate_no) ON DELETE CASCADE,
+  FOREIGN KEY (member_no) REFERENCES `member`(member_no)
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `preference` (
+  `preference_no` int PRIMARY KEY auto_increment,
+  `title` varchar(1000) NOT NULL,
+  `description` text
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4;
