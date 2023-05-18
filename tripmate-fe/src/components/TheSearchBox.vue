c4b21dc5338806d8877b7801b2ff8e06f0658073
leeyongjun <jjuny7712@gmail.com>
2023-05-18 16:20:54 +0900

    Feat: SearchBox 구현

---
+++ tripmate-fe/src/components/TheSearchBox.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <b-icon
+            class="search-icon mr-1"
+            icon="search"
+            animation="throb"
+            font-scale="1.5"
+        ></b-icon>
+        <input class="search-form" placeholder="제목, 게시글 검색" value="" />
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.search-form {
+    width: 200px;
+    border: none;
+    outline: none;
+    padding: 0;
+    font-weight: 700;
+    font-size: 20px;
+}
+input {
+    width: 20rem;
+    height: 2.5rem;
+    outline: none;
+    font-size: 1rem;
+    padding: 0 1.5rem;
+    border: 3px solid #4482f7;
+    border-radius: 0.5rem;
+    margin-right: 0.5rem;
+}
+</style>
