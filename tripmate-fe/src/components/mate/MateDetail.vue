d46c33c522fbf84bab7acd4478cc2677070fe029
minjeong.kim512 <minjeong.kim512@gmail.com>
2023-05-19 17:28:37 +0900

    [Comment] detail 주석 해제

--- tripmate-fe/src/components/mate/MateDetail.vue
+++ tripmate-fe/src/components/mate/MateDetail.vue
@@ -7,46 +7,6 @@
         </b-row>
         <mate-input-item type="detail" />
     </b-container>
-    <!-- <b-row class="mb-1">
-        <b-col style="text-align: left">
-            <b-form>
-
-                <b-form-group id="title-group" label="제목:" label-for="title">
-                    <b-form-input
-                        id="title"
-                        v-model="this.mateItem.title"
-                        type="text"
-                        readonly
-                    ></b-form-input>
-                </b-form-group>
-
-                <b-form-group
-                    id="content-group"
-                    label="내용:"
-                    label-for="content"
-                >
-                    <b-form-textarea
-                        id="content"
-                        v-model="this.mateItem.content"
-                        rows="10"
-                        max-rows="15"
-                        readonly
-                    ></b-form-textarea>
-                </b-form-group>
-
-                <b-button
-                    type="submit"
-                    variant="primary"
-                    class="m-1"
-                    @click="moveModify"
-                    >수정</b-button
-                >
-                <b-button @click="deleteMateItem" variant="danger" class="m-1"
-                    >삭제</b-button
-                >
-            </b-form>
-        </b-col>
-    </b-row> -->
 </template>
 
 <script>
